41c1966ffdc2f79a621550b22fdcded247279a99
muroslav2909 <myroslav@preply.com>
2017-04-16 22:16:48 +0300

    fewf

--- lowcost/manager.py
+++ lowcost/manager.py
@@ -9,7 +9,7 @@ from opencostavia.settings import BASE_DIR
 
 
 def setUp():
-    display = Display(visible=0, size=(800, 600))
+    display = Display(visible=0, size=(1024, 768))
     display.start() # for server should be uncomented
     driver = webdriver.Chrome()
     driver.implicitly_wait(30)
@@ -39,7 +39,7 @@ def button_click(driver):
         select.select_by_visible_text(u'травень 2017')
         sleep(2)
     except:
-        driver.save_screenshot(BASE_DIR + str(datetime.now()) + '.png')
+        driver.save_screenshot("%s_%s.png" % ('/lowcost/screenshots/', str(datetime.now()).replace(' ','')))
 
 def show_time(driver):
     if driver.find_element_by_xpath('//*[@id="app"]/div[3]/div/main/div/div/div[2]/div[2]/div[1]/div[2]/div/button').text == u"ПОКАЗАТИ ЧАС ВІДПРАВЛЕННЯ":
