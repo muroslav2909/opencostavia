a9ee3b2197d0485f3dd36fcbffaf77902f916c68
muroslav2909 <myroslav@preply.com>
2017-04-16 14:00:22 +0300

    stat

--- opencostavia/urls.py
+++ opencostavia/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+import os
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls.static import static
@@ -23,3 +24,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
               # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+SERVER_ENVIRONMENT = os.getenv('RUN_ENV', '')
+if SERVER_ENVIRONMENT == 'PROD':
+    urlpatterns += url('', (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}), )
