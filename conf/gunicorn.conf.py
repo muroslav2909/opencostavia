b910fff40776d69f709469dbcd715cc195800d2c
muroslav2909 <myroslav@preply.com>
2017-04-16 19:34:26 +0300

    cel2

---
+++ conf/gunicorn.conf.py
@@ -0,0 +1,3 @@
+import multiprocessing
+bind = "0.0.0.0:8000"
+workers = multiprocessing.cpu_count() * 2 + 1
--- redeploy.sh
+++ redeploy.sh
@@ -1,9 +1,23 @@
-python manage.py supervisor stop all
-kill -9 `ps -ef | grep supervisor | grep -v grep | awk '{print $2}'`
-#sudo /etc/init.d/nginx stop
-#sudo git pull origin master
-pip install -r requirements.txt
-python manage.py migrate
-python manage.py collectstatic --noinput --clear
-#sudo service nginx start
+#!/bin/bash
+
+sudo kill -9 `ps -ef | grep supervisor | grep -v grep | awk '{print $2}'`
+sudo kill -9 `ps -ef | grep gunicorn | grep -v grep | awk '{print $2}'`
+sudo kill -9 `ps -ef | grep celery | grep -v grep | awk '{print $2}'`
+
+
+if [ "$RUN_ENV" == PROD ]; then
+    sudo kill -9 `ps -ef | grep nginx | grep -v grep | awk '{print $2}'`
+    sudo git pull origin master
+    pip install -r requirements.txt
+    python manage.py migrate
+    python manage.py collectstatic --noinput --clear
+    sudo service nginx restart
+fi
+
+
 python manage.py supervisor -d
+
+
+
+
+
